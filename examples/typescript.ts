307e36f3eaad77418504ec02c319d9937382ea37
Scalingapps <scalingvue@gmail.com>
2020-08-22 09:07:17 -0400

    typescript example

---
+++ examples/typescript.ts
@@ -0,0 +1,23 @@
+import { PermissionType, PermissionsBuilder } from '@/sa-permissions'
+import { HasPermissionsArgs, AddUserPermissionsArgs, Security } from '@/sa-security'
+
+// setup
+const appSecurity = new Security()
+// this is to help tests, but you would have the value stored in a db or alike
+const builder = new PermissionsBuilder(PermissionType)
+
+// add user permissions
+appSecurity.addUserPermissions({ // argument is of type AddUserPermissionsArgs
+	userId: userId,
+	permissions: {
+		Items: builder.fromKeys(['View', 'Add']),
+		Accounts: builder.fromKeys(['View']),
+	},
+})
+
+// check for permissions
+appSecurity.hasPermissions({ // argument is of type HasPermissionsArgs
+	userId: 'test-user',
+	domain: 'Items',
+	permissionType: PermissionType.View,
+})
